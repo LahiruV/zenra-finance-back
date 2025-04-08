54401e88e1e0a704d5a52a265e7b3f10a2d4681f
Lahiru Vimukthi <67822002+LahiruV@users.noreply.github.com>
2025-04-08 15:11:50 +0530

    Refactor ExpenseController to implement AddExpense and GetExpense methods with proper validation and response handling

--- zenra-finance-back/Controllers/ExpenseController.cs
+++ zenra-finance-back/Controllers/ExpenseController.cs
@@ -2,14 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using zenra_finance_back.Models;
+using zenra_finance_back.Services.IServices;
 
 namespace zenra_finance_back.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class ExpenseController : ControllerBase
     {
-        
+        private readonly IExpenseService _service;
+        public ExpenseController(IExpenseService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("AddExpense")]
+        public async Task<IActionResult> AddExpense([FromBody] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _service.AddExpense(expense);
+                if (response.IsSuccess)
+                {
+                    return Ok(response);
+                }
+                return BadRequest(response);
+            }
+            return BadRequest("Some properties are not valid");
+        }
+
+        [HttpGet("GetExpense")]
+        public async Task<IActionResult> GetExpense()
+        {
+            var response = await _service.GetExpense();
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+
     }
 }
