e59f8b4e76b18a1b832ab66bd1cb4fc6c1bbdda3
Lahiru Vimukthi <lvimukthi+psfoods@peerlessfoods.com.au>
2025-04-11 19:16:04 +0530

    Implement token validation in UserService after generating JWT token

--- zenra-finance-back/Services/TokenService.cs
+++ zenra-finance-back/Services/TokenService.cs
@@ -6,15 +6,22 @@ using System.Threading.Tasks;
 using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using zenra_finance_back.Data;
 using zenra_finance_back.Services.IServices;
 
 namespace zenra_finance_back.Services
 {
     public class TokenService
     {
+        private readonly AppDbContext _context;
         public async Task<string> GenerateToken(User user)
         {
             return await Task.FromResult(GenerateJwtToken(user));
+        }
+
+        public async Task<int> ValidateToken(string token)
+        {
+            return await Task.FromResult(GetUserByToken(token));
         }
 
         private string GenerateJwtToken(User user)
@@ -38,6 +45,18 @@ namespace zenra_finance_back.Services
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private int GetUserByToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+            var userIdClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            if (userIdClaim != null)
+            {
+                return int.Parse(userIdClaim.Value);
+            }
+            throw new Exception("Invalid token");
         }
 
     }
